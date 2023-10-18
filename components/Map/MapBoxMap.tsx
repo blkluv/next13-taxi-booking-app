320e5af4c67630a357aa5476563eabd1ff327411
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2023-10-18 19:47:00 -0400

    Added a Marker provided by the MabBox to pinpoint the user's location

--- components/Map/MapBoxMap.tsx
+++ components/Map/MapBoxMap.tsx
@@ -1,23 +1,37 @@
 "use client";
-import React from 'react'
-import { Map } from 'react-map-gl';
+import { UserLocationContext } from '@/context/UserLocationContext';
+import React, { useContext } from 'react'
+import { Map, Marker } from 'react-map-gl';
+// Styling for the marker
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 const MapBoxMap = () => {
 
+  // useUser() from Clerk can be used to get the user's profile and display it on the map inside the marker/pointer
+
+  const { userLocation, setUserLocation } = useContext(UserLocationContext);
   return (
     <div className="px-5">
       <div className="rounded-lg overflow-hidden">
       <h2 className="text-[20px] font-semibold text-center text-2xl ">Map</h2>
-        <Map
+        {userLocation ? <Map
           mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
           initialViewState={{
-            longitude: -122.4,
-            latitude: 37.8,
+            longitude: userLocation.lng,
+            latitude: userLocation.lat,
             zoom: 14
           }}
           style={{width: '100%', height: 800, borderRadius: 10}}
           mapStyle="mapbox://styles/mapbox/streets-v9"
-        />
+        >
+          <Marker longitude={userLocation.lng} latitude={userLocation.lat} anchor="bottom" >
+            <img 
+              src="./pin.png" 
+              className='w-10 h-10'
+            />
+          </Marker>
+        </Map>
+        : null}
       </div>
     </div>
   )
