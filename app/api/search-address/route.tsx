14a2758b2597b2794df6eb2d51232f28904ccbc5
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2023-10-17 23:19:50 -0400

    Configured Mapbox API with the NextJS app

---
+++ app/api/search-address/route.tsx
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+
+const { uuid } = require('uuidv4');
+
+
+const session_token = uuid();
+console.log(session_token);
+
+
+const BASE_URL = 'https://api.mapbox.com/search/searchbox/v1/suggest';
+
+export async function GET(request: any) {
+    // Fetch the URL
+    const { searchParams } = new URL(request.nextUrl);
+    // Extract the query parameter from the URL
+    const searchText = searchParams.get('q');
+
+    const res = await fetch(BASE_URL+'?q='+searchText+'?language=en&limit=8&session_token='+session_token+'&country=US&access_token='+process.env.MAPBOX_ACCESS_TOKEN,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+    const searchResult = await res.json();
+
+    return NextResponse.json(searchResult)
+}
--- components/Booking/index.tsx
+++ components/Booking/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 ;import AutoComplete from './AutoComplete'
 
 const Booking = () => {
