e926dfa976fedcb8e92069cfcedfe4c2d7e53311
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2023-10-20 18:30:21 -0400

    Configured the map to fly over to the  Source and Destination markers respectively, when any of those are selected from address suggestion dropdown

--- components/Map/MapBoxMap.tsx
+++ components/Map/MapBoxMap.tsx
@@ -1,20 +1,53 @@
 "use client";
 import { UserLocationContext } from '@/context/UserLocationContext';
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import { Map, Marker } from 'react-map-gl';
 // Styling for the marker
 import 'mapbox-gl/dist/mapbox-gl.css';
+import Markers from './Markers';
+import { SourceCoordinatesContext } from '@/Context/SourceCoordinatesContext';
+import { DestinationCoordinatesContext } from '@/Context/DestinationCoordinatesContext';
 
 const MapBoxMap = () => {
 
   // useUser() from Clerk can be used to get the user's profile and display it on the map inside the marker/pointer
 
+  const mapRef = useRef<any>(null);
   const { userLocation, setUserLocation } = useContext(UserLocationContext);
+  const { sourceCoordinates, setSourceCoordinates } = useContext<any>(SourceCoordinatesContext);
+  const { destinationCoordinates, setDestinationCoordinates } = useContext<any>(DestinationCoordinatesContext);
+
+  //Fly over to the Source or Destination coordinates when they are selected from the address suggestion dropdown
+  useEffect(() => {
+    if(sourceCoordinates){
+      mapRef.current?.flyTo({
+        center: [
+          sourceCoordinates.lat,
+          sourceCoordinates.lng
+        ],
+        duration: 2500
+      });
+    }
+  },[sourceCoordinates]);
+
+  useEffect(() => {
+    if(destinationCoordinates){
+      mapRef.current?.flyTo({
+        center: [
+          destinationCoordinates.lat,
+          destinationCoordinates.lng
+        ],
+        duration: 2500
+      });
+    }
+  },[destinationCoordinates]);
+
   return (
     <div className="px-5">
       <div className="rounded-lg overflow-hidden">
       <h2 className="text-[20px] font-semibold text-center text-2xl ">Map</h2>
         {userLocation ? <Map
+          ref={mapRef}
           mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
           initialViewState={{
             longitude: userLocation.lng,
@@ -24,12 +57,7 @@ const MapBoxMap = () => {
           style={{width: '100%', height: 800, borderRadius: 10}}
           mapStyle="mapbox://styles/mapbox/streets-v9"
         >
-          <Marker longitude={userLocation.lng} latitude={userLocation.lat} anchor="bottom" >
-            <img 
-              src="./pin.png" 
-              className='w-10 h-10'
-            />
-          </Marker>
+          <Markers />
         </Map>
         : null}
       </div>
---
+++ components/Map/Markers.tsx
@@ -0,0 +1,58 @@
+import { DestinationCoordinatesContext } from '@/Context/DestinationCoordinatesContext';
+import { SourceCoordinatesContext } from '@/Context/SourceCoordinatesContext';
+import { UserLocationContext } from '@/context/UserLocationContext';
+import React, { useContext } from 'react'
+import { Map, Marker } from 'react-map-gl'
+
+const Markers = () => {
+
+    const { userLocation, setUserLocation } = useContext(UserLocationContext);
+    const { sourceCoordinates, setSourceCoordinates } = useContext<any>(SourceCoordinatesContext);
+    const { destinationCoordinates, setDestinationCoordinates } = useContext<any>(DestinationCoordinatesContext);
+
+    return (    
+        <div className="">
+            {/* User Marker */}
+            <Marker 
+                longitude={userLocation.lng} 
+                latitude={userLocation.lat} 
+                anchor="bottom" 
+            >
+                <img 
+                    src="./pin.png" 
+                    className='w-10 h-10'
+                />
+            </Marker>
+
+            {/* Source Marker */}
+            { sourceCoordinates.length ?<Marker 
+                longitude={sourceCoordinates.lng} 
+                latitude={sourceCoordinates.lat} 
+                anchor="bottom" 
+            >
+                <img 
+                    src="./pin.png" 
+                    className='w-10 h-10'
+                />
+            </Marker>
+            :
+            null}
+
+            {/* Destination Marker */}
+            { destinationCoordinates.length ?<Marker 
+                longitude={destinationCoordinates.lng} 
+                latitude={destinationCoordinates.lat} 
+                anchor="bottom" 
+            >
+                <img 
+                    src="./pin.png" 
+                    className='w-10 h-10'
+                />
+            </Marker>
+        :
+        null}
+        </div>
+    )
+}
+
+export default Markers
