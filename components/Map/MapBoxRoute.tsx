66b65ccc410efe34403c2c5ab3b08233c24d8b23
BLK LUV [org] <info@blkluv.org>
2023-10-21 05:02:54 -0400

    Update MapBoxRoute.tsx

--- components/Map/MapBoxRoute.tsx
+++ components/Map/MapBoxRoute.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Layer, Source } from 'react-map-gl'
-// Layer allows to draw anything on the map
+import React from 'react';
+import { Layer, Source } from 'react-map-gl';
 
 const MapBoxRoute = (props: any) => {
     return (
-        <Source 
-            type='geojson'
+        <Source
+            type="geojson"
             data={{
                 type: 'Feature',
+                properties: {}, // You need to include an empty properties object
                 geometry: {
                     type: 'LineString',
-                    coordinates: props.coordinates
-                }
+                    coordinates: props.coordinates,
+                },
             }}
         >
-            <Layer 
-                type='line'
-                layout={{ "line-join": "round", "line-cap": "square" }}
-                paint={{ "line-color": "#0462d4", "line-width": 4 }}
+            <Layer
+                type="line"
+                layout={{ 'line-join': 'round', 'line-cap': 'round' }} // Changed 'square' to 'round'
+                paint={{ 'line-color': '#0462d4', 'line-width': 4 }}
             />
         </Source>
-    )
-}
+    );
+};
 
-export default MapBoxRoute
+export default MapBoxRoute;
